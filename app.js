1857ba525143821c581eb55fa382475771ef153f
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-19 16:47:06 -0500

    environment variables

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const app = express();
 
 ////////////////////////////////
 // MIDDLEWARES
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
