23565f782d090e5560530f68e7e0b2f97df55aaf
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-19 11:27:00 -0500

    handling GET requests in nodejs

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from the server", app: "Hola" });
-});
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Hello from the server", app: "Hola" });
+// });
 
-app.post("/", (req, res) => {
-  res.send("You can post");
+// app.post("/", (req, res) => {
+//   res.send("You can post");
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
