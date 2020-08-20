d26fe497d08fadeec5e4f808c52575ac12bd9cb2
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-19 21:55:06 -0500

    connected local database with mongodb atlas, installed mongoose and created a simple model

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const app = require("./app");
 
 dotenv.config({ path: "./config.env" });
 
-const app = require("./app");
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
