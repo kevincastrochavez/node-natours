4d3cdf2216f313318a8b222eb031bcc3c13ae71c
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-20 16:21:01 -0500

    filtering, excluiding fields in the query and executing it

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,8 +13,25 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((field) => delete queryObj[field]);
 
+    console.log(req.query, queryObj);
+
+    const query = await Tour.find(queryObj);
+
+    // const tours = Tour.find()
+    //   .where("duration")
+    //   .equals(5)
+    //   .where("difficulty")
+    //   .equals("easy");
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SENT RESPONSE
     res.status(200).json({
       status: "success",
       results: tours.length,
