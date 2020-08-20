d97322d198173bb1ebd47178018df567fad05a0a
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-20 12:12:29 -0500

    updating documents with mongoos

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -66,13 +66,20 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "Updated tour",
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour: tour,
+      },
+    });
+  } catch (err) {}
 };
 
 exports.deleteTour = (req, res) => {
