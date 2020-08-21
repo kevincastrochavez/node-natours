416093c42dad6502a9e8363939c3c6db6d74689b
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-21 12:34:13 -0500

    learning statistics, matching and grouping

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,6 +10,8 @@ router
   .route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTours); // Route for top 5 tours
 
+router.route("/tour-stats").get(tourController.getTourStats);
+
 router
   .route("/")
   .get(tourController.getAllTours)
