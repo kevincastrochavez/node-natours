bd6e6da53a4f707c88a7fce5596d15df08928488
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-22 12:19:34 -0500

    refactor error handling, created errorHandler class and imported it in app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -33,27 +35,13 @@ app.use("/api/v1/user", userRouter);
 
 // Unhandled routes
 app.all("*", (req, res, next) => {
-  // res.status(404).json({
-  //   status: "fail",
-  //   message: `Can not find ${req.originalUrl} on this server`,
-  // });
-  // next();
+  // const err = new Error("Can not fin in server");
+  // err.status = "fail";
+  // err.statusCode = 404;
 
-  const err = new Error("Can not fin in server");
-  err.status = "fail";
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError("Can not fin in server", 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
