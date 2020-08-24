d50fe7fef3d727dfc604dac344da7102a60321be
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-24 13:22:18 -0500

    learning how to login users with token

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const authenticationController = require("../controllers/authenticationControlle
 const router = express.Router();
 
 router.post("/signup", authenticationController.signup);
+router.post("/login", authenticationController.login);
 
 router
   .route("/")
