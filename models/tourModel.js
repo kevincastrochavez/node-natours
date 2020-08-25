1678304ff6e39623b76764bc6db8d7ddfbc791ea
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-25 16:58:25 -0500

    populating tour guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -154,6 +154,15 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "guides",
+    select: "-__v0 -passwordChangedAt",
+  });
+
+  next();
+});
+
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start}`);
   next();
