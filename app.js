94dc1c4f669eb4d0b25ccb6e91af415f2de7713c
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-25 13:04:25 -0500

    installed helmet package and set security http header

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -12,19 +13,26 @@ const app = express();
 
 ////////////////////////////////
 // GLOBAL MIDDLEWARES
+// SET SECURITY HTTP HEADERS
+app.use(helmet());
+
+// DEVELOPMENT LOGGING
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// LIMIT REQUESTS FOR SAME API BY IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP. Please try again in an hour",
 });
-
 app.use("/api", limiter);
 
-app.use(express.json());
+// BODY PARSER
+app.use(express.json({ limit: "10kb" }));
+
+// SERVING STATIC FILES
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
@@ -32,6 +40,7 @@ app.use(express.static(`${__dirname}/public`));
 //   next();
 // });
 
+// TEST MIDDLEWARE
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
