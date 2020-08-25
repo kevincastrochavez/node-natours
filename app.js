3f93da4ab916b10401ae7f81fa4d0788c300678b
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-25 13:16:04 -0500

    installed xss-clean and express-mongo-sanitize packages and used them

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -31,6 +33,12 @@ app.use("/api", limiter);
 
 // BODY PARSER
 app.use(express.json({ limit: "10kb" }));
+
+// DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+// DATA SANITIZATION AGAINST XSS
+app.use(xss());
 
 // SERVING STATIC FILES
 app.use(express.static(`${__dirname}/public`));
