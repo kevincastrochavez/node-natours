113e50cbc94dc637ab81ca70d1f9a8657aa0b6b6
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-26 11:54:32 -0500

    improved reading performance with indexes

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -76,7 +76,8 @@ exports.getAll = (Model) =>
       .sort()
       .limitFields()
       .paginate();
-    const doc = await features.query;
+    // const doc = await features.query.explain();
+    const doc = await features.query.explain();
 
     // SENT RESPONSE
     res.status(200).json({
